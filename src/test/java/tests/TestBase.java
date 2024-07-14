b2b6dbc3f6b990e0252cd340daf232011b9fe786
Tatiana Stulova <vitadbnf@mail.ru>
2024-07-14 20:33:24 +0300

    change BooksStoreTests

--- src/test/java/tests/TestBase.java
+++ src/test/java/tests/TestBase.java
@@ -25,14 +25,14 @@ public class TestBase {
         Configuration.browserSize = System.getProperty("browserSize", "1920x1080");
         Configuration.browserVersion = System.getProperty("browserVersion", "121");
        // Configuration.timeout = 10000;
-        Configuration.remote = "https://user1:1234@"+ System.getProperty("selenoid","selenoid.autotests.cloud")+"/wd/hub";
-       /* Configuration.remote = "https://"
+        //Configuration.remote = "https://user1:1234@"+ System.getProperty("selenoid","selenoid.autotests.cloud")+"/wd/hub";
+       Configuration.remote = "https://"
                 + System.getProperty("login")
                 + ":"
                 + System.getProperty("pass")
                 + "@"
-                + System.getProperty("host")
-                + "/wd/hub";*/
+                + System.getProperty("selenoid")
+                + "/wd/hub";
 
         DesiredCapabilities capabilities = new DesiredCapabilities();
         capabilities.setCapability("selenoid:options", Map.<String, Object>of(
